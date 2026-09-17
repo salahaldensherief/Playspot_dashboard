9108c289af8f334bbd392abffdcd78b48bc3c7e6
SalahEldin Sheref <salaheldindev11@gmail.com>
2026-09-17 09:59:55 +0300

    add debouncer utility, implement debouncing for search inputs in booking and loyalty filters, and update data fetching logic in booking and dashboard data sources

--- lib/features/bookings/presentation/cubit/booking_cubit.dart
+++ lib/features/bookings/presentation/cubit/booking_cubit.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../../core/audio/audio_service.dart';
 import '../../domain/entities/booking.dart';
 import '../../domain/usecases/confirm_cash_payment.dart';
